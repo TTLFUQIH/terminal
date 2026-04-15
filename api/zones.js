64c058a0e271f1f2b9cb5de5cfb3bbc8935bf779
TTLFUQIH <nahuelvitullo@hotmail.com>
2026-04-15 01:05:03 -0300

    cambios

--- api/zones.js
+++ api/zones.js
@@ -1,4 +1,5 @@
-import sql from './db.js';
+import { neon } from '@neondatabase/serverless';
+const sql = neon(process.env.DATABASE_URL);
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
